2cbd73d4b988f8cc61c04b3aac391d527a488a76
Nagacharan.K <nagacharan.k@appliedsynergy.tech>
2021-06-20 17:00:05 +0530

    breadcrums color

--- application/views/404.php
+++ application/views/404.php
@@ -1,14 +1,16 @@
 <style>
     #notfound {
   position: relative;
-  height: 80vh;
+  color:#fff;
+  height: 60vh;
 }
 .category-header-area{
-  background-color: #171a1d;
+  background-color: #242B2E;
 }
 #notfound .notfound {
   position: absolute;
   left: 50%;
+  color:#fff;
   top: 50%;
   -webkit-transform: translate(-50%, -50%);
       -ms-transform: translate(-50%, -50%);
@@ -18,20 +20,23 @@
 .notfound {
   max-width: 520px;
   width: 100%;
+  color:#fff;
   text-align: center;
   line-height: 1.4;
 }
 
 .notfound .notfound-404 {
   height: 190px;
+  color:#fff;
 }
 
 .notfound .notfound-404 h1 {
 
   font-size: 146px;
+  color:#fff;
   font-weight: 700;
   margin: 0px;
-  color: #fff;
+ 
 }
 
 .notfound .notfound-404 h1>img{
@@ -46,28 +51,26 @@
 }
 
 .notfound h2 {
-
   font-size: 22px;
   font-weight: 700;
   margin: 0;
+  color:#fff;
   text-transform: uppercase;
-  color: #fff;
 }
 
 .notfound p {
-
-  color: #fff;
+  color:#fff;
   font-weight: 300;
 }
 
 .notfound a {
 
   display: inline-block;
-  padding: 12px 30px;
+  /* padding: 12px 30px; */
   font-weight: 700;
-  background-color: #122179;
-  color: #fff;
-  border-radius: 40px;
+  /* background-color: #122179; */
+color:#fff;
+  /* border-radius: 40px; */
   text-decoration: none;
   -webkit-transition: 0.2s all;
   transition: 0.2s all;
@@ -96,9 +99,9 @@
 			<div class="notfound-404">
 				<h1>4<img alt="0" src="<?= base_url('assets/images/icon/emoji.png')?>"/>4</h1>
 			</div>
-			<h2>Oops! Page Not Be Found</h2>
-			<p>Sorry but the page you are looking for does not exist, have been removed, name changed or is temporarily unavailable</p>
-			<a href="<?php echo site_url(); ?>">back to home</a>
+			<h2 class="my-1">Oops! Page Not Found</h2>
+			<p class="my-2">Sorry but the page you are looking for does not exist, have been removed, name changed or is temporarily unavailable</p>
+			<a href="<?php echo site_url(); ?>"><u>back to home</u></a>
 		</div>
 	</div>
 </section>
